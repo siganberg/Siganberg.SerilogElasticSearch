2765b1965c67e794c9a35c71d6428a7c6bbb3654
Francis <francis.marasigan@iherb.om>
2020-08-07 22:30:49 -0700

    Updated nuget version #

--- src/publish.sh
+++ src/publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dotnet nuget push Siganberg.SirilogElasticSearch/bin/Release/Siganberg.SirilogElasticSearch.$1.nupkg -k $2 -s "https://api.nuget.org/v3/index.json"
+dotnet nuget push Siganberg.SirilogElasticSearch/bin/Release/Siganberg.SirilogElasticSearch.$1.nupkg -k $NUGETKEY -s "https://api.nuget.org/v3/index.json"
